29461253dffe099c013940ef0cd40452ea0827c8
Eric Marty <eric.marty@genistat.ch>
2019-01-06 22:59:07 +0100

    Refining code modularity and adding Brewfile

--- install.sh
+++ install.sh
@@ -1,66 +1,25 @@
 #!/usr/bin/env bash
 
+DOTFILES_REPO=~/dotfiles
+
 function main() {
-    ########################################
-    header "Introduction" "Hello there"
-    ########################################
+    header "Hello there"
     echo "This script installs programs and sets up the configurations on you machine."
     echo "It is idempotent so it can be run multiple times without causing issues."
     echo "In the beginning there might be some things which require user interaction, so beware."
     echo "I will tell you as soon as you can lay back."
-
-
-    ########################################
-    header "Prescript" "Downloading dotfiles repository from GitHub"
-    ########################################
-
-    # Command Line Tools
-    xcode-select -p > /dev/null 2>&1
-    if [[ $? != 0 ]]; then
-        info "Installing Command Line Tools"
-        info "This will prompt you with a software update pop-up, simply press \"Install\""
-        xcode-select --install
-        read -p "Wait for the installation to finish, then press [ENTER] to continue..."
-        ok
-    fi
-
-    git clone git@github.com:martyer/dotfiles.git
-
     echo
-    ########################################
-    header "Installation" "Checking Homebrew installation"
-    ########################################
 
-    # Installing Homebrew
-    which brew > /dev/null 2>&1
-    if [[ $? != 0 ]]; then
-        info "Installing Homebrew"
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        if [[ $? != 0 ]]; then
-          error "Unable to install Homebrew"
-          exit 1
-        fi
-        ok
-    fi
+    ask_for_sudo
+    install_homebrew
 
-    # Updating Homebrew
-    info "Updating Homebrew"
-    brew update
-    ok
-
-    # Updating packages
-    info "Updating packages"
-    brew upgrade
-    ok
-
-
+    header "Woop, woop..."
+    echo "Now there should be no more user interaction required. Lay back and relax."
     echo
-    ########################################
-    header "Configuration" "Defaults write"
-    ########################################
 
-    source ./defaults.sh
-
+    clone_dotfiles_repo
+    install_packages_with_brewfile
+    setup_macOS_defaults
 
     # Setting git user name und email address
     if [[ ! -f ~/.gitconfig  ]]; then
@@ -76,17 +35,94 @@ function main() {
         git config --global user.email "$email"
     fi
 
-
-
     # TODO add the private keys to PWManager
 }
 
-################################################################################
-# Helper functions for beautified console output
+function ask_for_sudo() {
+    header "Prompting for sudo password"
+    if sudo --validate; then
+        # Keep-alive
+        while true; do sudo --non-interactive true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+        ok "Sudo credentials acquired"
+    else
+        error "Obtaining sudo credentials failed"
+        exit 1
+    fi
+    echo
+}
+
+function install_homebrew() {
+    header "Installing Homebrew"
+    if hash brew 2>/dev/null; then
+        ok "Homebrew already installed"
+    else
+        if /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; then
+            ok "Homebrew installation succeeded"
+        else
+            error "Homebrew installation failed"
+            exit 1
+        fi
+    fi
+    echo
+}
+
+function clone_dotfiles_repo() {
+    header "Cloning dotfiles repository into $DOTFILES_REPO"
+    if test -e "$DOTFILES_REPO"; then
+        info "$DOTFILES_REPO already exists"
+        info "Pulling latest changes in $DOTFILES_REPO repository"
+        if git -C "$DOTFILES_REPO" pull origin master &> /dev/null; then
+            return
+        else
+            error "Error while pulling, check the $DOTFILES_REPO repo manually"
+            exit 1
+        fi
+        ok "Pull successful in $DOTFILES_REPO repository"
+    else
+        url=https://github.com/martyer/dotfiles.git
+        if git clone "$url" "$DOTFILES_REPO"; then
+            ok "Cloned into $DOTFILES_REPO"
+        else
+            error "Cloning into $DOTFILES_REPO failed"
+            exit 1
+        fi
+    fi
+    echo
+}
+
+function install_packages_with_brewfile() {
+    BREW_FILE="$DOTFILES_REPO/Brewfile"
+
+    header "Installing packages within $BREW_FILE"
+    if brew bundle check --file="$BREW_FILE" &> /dev/null; then
+        ok "All packages already up to date"
+    else
+        if brew bundle --file="$BREW_FILE"; then
+            ok "Installation of packages succeeded"
+        else
+            error "Installation of packages failed"
+            exit 1
+        fi
+    fi
+    echo
+}
+
+function setup_macOS_defaults() {
+    DEFAULTS_SCRIPT="$DOTFILES_REPO/defaults.sh"
+
+    header "Updating macOS defaults"
+    if source "$DEFAULTS_SCRIPT"; then
+        ok "Defaults setup succeeded"
+    else
+        error "Defaults setup failed"
+        exit 1
+    fi
+    echo
+}
 
 function header() {
     # Bold
-    echo -e "\033[1m$1\033[0m - $2"
+    echo -e "\033[1mʘ‿ʘ\033[0m - $1"
 }
 
 function info() {
@@ -107,20 +143,6 @@ function warn() {
 function error() {
     # Red and Bold
     echo -e "\033[31;1m[ERROR]\033[0m $1"
-}
-################################################################################
-
-function brew_install_or_upgrade {
-    if brew ls --versions "$1" >/dev/null; then
-    if (brew outdated | grep "$1" > /dev/null); then
-        echo "Upgrading already installed package $1 ..."
-        brew upgrade "$1"
-    else
-        echo "Latest $1 is already installed"
-    fi
-    else
-    brew install "$1"
-    fi
 }
 
 main "$@"
